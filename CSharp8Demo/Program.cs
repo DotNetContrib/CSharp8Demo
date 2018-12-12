9cb65e772518f495f2add0d736a8994d86c2f26b
Nithin Mohan T K <nithinmohantk@nitrix-reloaded.com>
2018-12-12 20:32:27 +0000

    Add project files.

---
+++ CSharp8Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CSharp8Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IVehicle bmw = new Bmw();
+            bmw.DefaultMessage();
+
+            IVehicle audi = new Audi();
+            audi.DefaultMessage(); 
+        }
+    }
+
+
+    interface IVehicle
+    {
+        //default implementation 
+        void DisplayMessage();
+        public void DefaultMessage() => Console.WriteLine("I am  inside default method in the interface!");
+      
+    }
+
+    public class Bmw : IVehicle
+    {
+        public void DisplayMessage()
+        {
+            Console.WriteLine("I am BMW!!!");
+        }
+    }
+
+    public class Audi : IVehicle
+    {
+        public void DisplayMessage()
+        {
+            Console.WriteLine("I am AUDI!!!");
+        }
+        public void DefaultMessage() => Console.WriteLine("I am  inside audi class!");
+    }
+}
